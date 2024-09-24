9f1db8972183bdf2d651a08cb90333dfdb37d9e8
Ukkojid <130745930+Ukkojid@users.noreply.github.com>
2024-09-24 21:04:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
